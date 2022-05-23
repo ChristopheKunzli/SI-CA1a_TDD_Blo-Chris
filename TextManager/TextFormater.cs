15f941ae11644c542f0a04679ae52d4506b53f42
KUNZLI Christophe <Christophe.KUNZLI@cpnv.ch>
2022-05-23 08:51:22 +0200

    Passed T001

--- TextManager/TextFormater.cs
+++ TextManager/TextFormater.cs
@@ -8,7 +8,25 @@ namespace TextManager
         #region public methods
         public string Reverse(string textToReverse)
         {
-            throw new NotImplementedException();
+            //Format
+            textToReverse = textToReverse.Remove(textToReverse.Length - 1, 1);
+            textToReverse = textToReverse.ToLower();
+
+            //Reverse sentence
+            string[] splitted = textToReverse.Split(" ");
+            string empty = "";
+            for (int i = splitted.Length -1; i >= 0; i--)
+            {
+                empty += splitted[i] + " ";
+            }
+
+            //Remove extra space
+            empty = empty.Remove(empty.Length - 1, 1);
+
+            empty = char.ToUpper(empty[0]) + empty;
+            empty = empty.Remove(1, 1);
+            empty += ".";
+            return empty;
         }
         #endregion public methods
 
