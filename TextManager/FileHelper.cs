ec196f8ec345b742d94770693ec9862e1c4df1d5
PabloZubieta <93717372+PabloZubieta@users.noreply.github.com>
2022-06-08 13:38:41 +0200

    filehelper updated

---
+++ TextManager/FileHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextManager
+{
+    public class FileHelper
+    {
+        #region public methods
+        public string Import(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion public methods
+    }
+}
---
+++ TextManager/TestTextManager/TestFileHelper.cs
@@ -0,0 +1,35 @@
+using NUnit.Framework;
+using TextManager;
+
+namespace TestTextManager
+{
+    public class TestFileHelper
+    {
+        #region private attributes
+        private FileHelper fileHelper = null;
+        #endregion private attributes
+
+        [SetUp]
+        public void Setup()
+        {
+            this.fileHelper = new FileHelper();
+        }
+
+        #region Single import
+        [Test]
+        public void T001_NominalCase_Success()
+        {
+            //given
+            string filePath = "T001.txt";
+            string expectedResult = "Il faut agir aussi vite que possible.";
+            string actualResult;
+
+            //when
+            actualResult = (this.fileHelper.Import(filePath));
+
+            //then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        #endregion Single import
+    }
+}
